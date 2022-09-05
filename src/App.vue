3f5ac340c44a77ef3521abf03313cb0e0ceb4ea8
Ramil Nurullaev <sightlesssf46@gmail.com>
2022-09-06 00:28:51 +0400

    дз3

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header></header>
+    <main>
+      <expensesList :paymentsList="paymentsList"/>
+      <div class="expenses-form">
+        <button @click="addFormBtn">{{ addForm }} форму</button>
+        <expensesForm v-if="show" @add-expenses="addExpenses"/>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import expensesList from '@/components/ExpensesList'
+import expensesForm from '@/components/expensesForm'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    expensesList,
+    expensesForm
+  },
+  data: () => ({
+    paymentsList: [],
+    show: false,
+    addForm: 'показать'
+  }),
+  methods: {
+    fetchPaymentsData () {
+      return [
+        {
+          date: '28.03.2020',
+          category: 'Food',
+          value: 1488
+        },
+        {
+          date: '14.88.2002',
+          category: 'Food',
+          value: 1231
+        },
+        {
+          date: '32.22.2800',
+          category: 'Food',
+          value: 2033
+        }
+      ]
+    },
+    addExpenses (data) {
+      this.paymentsList.push(data)
+    },
+    addFormBtn () {
+      this.show = !this.show
+      !this.show ? this.addForm = 'показать' : this.addForm = 'скрыть'
+    }
+  },
+  created () {
+    setTimeout(() => {
+      this.paymentsList = this.fetchPaymentsData()
+    }, 0)
   }
 }
 </script>
@@ -24,5 +70,19 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+main{
+  display: flex;
+  justify-content: center;
+}
+button {
+  background-color: #32AB9B;
+  border: 0;
+  width: 100px;
+  height: 40px;
+  border-radius: 5px;
+}
+.expenses-form{
+  width: 250px;
 }
 </style>
---
+++ src/components/expensesForm.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="form">
+    <input placeholder="value" v-model="value">
+    <input placeholder="category" v-model="category">
+    <input placeholder="date" v-model="date">
+    <button @click="addExpenses">Add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'expensesForm',
+  data: () => ({
+    value: '',
+    category: '',
+    date: ''
+  }),
+  methods: {
+    addExpenses () {
+      const { value, category, date } = this
+      const data = {
+        value,
+        category,
+        // date: this.date ? this.date : this.currentDay
+        date: date || this.currentDay
+      }
+      this.$emit('add-expenses', data)
+    }
+  },
+  computed: {
+    currentDay () {
+      const currentDate = new Date()
+      const day = currentDate.getDate()
+      const month = currentDate.getMonth() + 1
+      const year = currentDate.getFullYear()
+      return `${day}.${month}.${year}`
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.form{
+  margin: 20px 0 0 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+}
+input{
+  width: 200px;
+  height: 40px;
+  border: 0px;
+  box-shadow: 4px 4px 50px 0px rgba(36, 132, 206, 0.2);
+  border-radius: 10px;
+  text-align: center;
+  outline: none;
+}
+button{
+  background-color: #32AB9B;
+  border: 0;
+  width: 100px;
+  height: 40px;
+  border-radius: 5px;
+  margin-left: 94px;
+}
+</style>
